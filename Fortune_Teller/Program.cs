aeb1d5b3a12f0b4b384c4c3adb521807c1b8e9b9
joderrick1022 <joderrick1022>
2017-09-29 14:39:02 -0400

    clead up unit two of project

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -10,6 +10,9 @@ namespace Fortune_Teller
     {
         static void Main(string[] args)
         {
+
+            
+
             Console.WriteLine("What is your first name?");
             string firstName = Console.ReadLine().ToLower();
 
@@ -22,11 +25,89 @@ namespace Fortune_Teller
             Console.WriteLine("What is your age? numbers only please EX(1,25,6,7,76)");
             int userAge = int.Parse(Console.ReadLine());
 
+            int retireYears = (userAge - 66);
+            bool odd = (userAge % 2 != 0); // these two bools are to check to see if user age is even olr odd.
+            bool even = (userAge % 2 == 0);
+            string fullName = firstName + lastName;
+
+            if (odd)
+            {
+
+                Console.WriteLine("you will retire in " + retireYears + " years" + "!");
+            }
+            else if (even)
+            {
+                Console.WriteLine("you will retire in " + retireYears + " years" + "!");
+            }
+
+
+
+           
+
             Console.WriteLine("What month were you born?");
             int birthMonth = int.Parse(Console.ReadLine());
 
             Console.WriteLine("How many siblings do you have?");
             int siblings = int.Parse(Console.ReadLine());
+
+
+            int zero = 0;
+            int one = 1;
+            int two = 2;
+            int three = 3;
+
+            //bool china = (siblings == 0);
+            //bool africa  = (siblings == 1);
+            //bool russia = (siblings == 2);
+            //bool florida = (siblings == 3);
+
+
+            
+
+
+
+
+            if (siblings == zero)
+            {
+                Console.WriteLine("Africa");
+
+            }
+
+            else if (siblings == one )
+            {
+                Console.WriteLine("Russia");
+
+            }
+
+            else if (siblings == two)
+            {
+                Console.WriteLine("North Pole \a");
+
+            }
+
+
+        
+
+            else if (siblings == three)
+            {
+                Console.WriteLine("Cancoon \a");
+
+            }
+           
+
+
+
+
+
+
+
+
+            {
+
+            }
+
+
+            
 
 
         }
