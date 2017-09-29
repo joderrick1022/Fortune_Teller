a478f91eedac9f861e8833ec4c5a183057efaa17
joderrick1022 <joderrick1022>
2017-09-29 12:15:59 -0400

    have my questins put in

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -10,6 +10,25 @@ namespace Fortune_Teller
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine().ToLower();
+
+            Console.WriteLine("what is your Last name? + \a");
+            string lastName = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your favorite ROYGBIV color? If need help understanding what ROYGBIV colors are please enter (help)");
+            string favColor = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your age? numbers only please EX(1,25,6,7,76)");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What month were you born?");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many siblings do you have?");
+            int siblings = int.Parse(Console.ReadLine());
+
+
         }
     }
 }
